fc529c5a342456d5ef68124544e0316f49b722c2
Mathys <mathys.laurent3164@gmail.com>
2023-07-26 17:21:37 +0000

    transpose a matrix with numpy

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""Transpose a NumPy matrix."""
+
+import numpy as np
+
+
+def np_transpose(matrix):
+    """
+    @param matrix - The matrix to transpose.
+
+    @return The transposed matrix.
+    """
+
+    trans = np.transpose(matrix)
+    return trans
