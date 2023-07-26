8b9aa5914923509a617f34a58e89126af9f90090
Mathys <mathys.laurent3164@gmail.com>
2023-07-26 16:51:40 +0000

    return the shape of a matrix

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+
+"""Return the shape of a NumPy matrix."""
+import numpy as np
+
+
+def np_shape(matrix):
+    """"
+    @param matrix - The matrix to be shaped. Must be a NumPy array.
+
+    @return The shape of the matrix as a tuple of integers.
+    """
+    return np.shape(matrix)
