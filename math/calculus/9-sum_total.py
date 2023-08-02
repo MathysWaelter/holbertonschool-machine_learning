32324c9d5637507c93ab7d7cfc50cbc7a4422551
Mathys <mathys.laurent3164@gmail.com>
2023-08-02 09:56:17 +0000

    sommation of i squared without numpy and without any loop

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -9,6 +9,6 @@ def summation_i_squared(n):
     @param n - number of integers to calculate
     @return sum of squares of
     """
-    if n < 0 or not isinstance(n, int):
+    if n < 1 or not isinstance(n, int):
         return None
     return int(n * (n + 1)*(2 * n + 1) / 6)
