c2736b5d16c12b4920980355cf63e036594b029f
Mathys <mathys.laurent3164@gmail.com>
2023-08-14 11:35:15 +0000

    change place of condition

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -5,20 +5,18 @@ import numpy as np
 
 
 class Neuron:
+    """ Initialize the parameters of the random walk."""
     def __init__(self, nx):
         """
-         Initialize the parameters of the random walk.
-
-         @param nx - number of nodes in the walk (must be positive)
+        @param nx - number of nodes in the walk (must be positive)
         """
-
-        self.W = np.random.randn(1, nx)
-        self.b = 0
-        self.A = 0
-
         # nx is an integer.
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
         # nx must be a positive integer
         if nx < 1:
             raise ValueError("nx must be a positive integer")
+
+        self.W = np.random.randn(1, nx)
+        self.b = 0
+        self.A = 0
