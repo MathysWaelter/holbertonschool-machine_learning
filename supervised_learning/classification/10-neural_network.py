612c23188ad5b2e24176706129ec26404c266fac
Mathys <mathys.laurent3164@gmail.com>
2023-08-21 13:11:36 +0000

    Forward propagation of the neural network.

---
+++ supervised_learning/classification/10-neural_network.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+
+"""defines a neural network with one hidden
+layer performing binary classification"""
+import numpy as np
+
+
+class NeuralNetwork:
+    """ Initialize the parameters of the random walk."""
+    def __init__(self, nx, nodes):
+        """
+        @param nx - number of nodes in the walk (must be positive)
+        """
+        # nx is an integer.
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        # nx must be a positive integer
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if not isinstance(nodes, int):
+            raise TypeError("nodes must be an integer")
+        # nodes must be a positive integer
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+
+        self.__W1 = np.random.randn(nodes, nx)
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+        self.__W2 = np.random.randn(1, nodes)
+        self.__b2 = 0
+        self.__A2 = 0
+
+    @property
+    def W1(self):
+        """
+        Getter for W1
+        """
+        return self.__W1
+
+    @property
+    def b1(self):
+        """
+        Getter for b1
+        """
+        return self.__b1
+
+    @property
+    def A1(self):
+        """
+        Getter for A1
+        """
+        return self.__A1
+
+    @property
+    def W2(self):
+        """
+        Getter for W2
+        """
+        return self.__W2
+
+    @property
+    def b2(self):
+        """
+        Getter for b2
+        """
+        return self.__b2
+
+    @property
+    def A2(self):
+        """
+        Getter for A2
+        """
+        return self.__A2
+
+    def forward_prop(self, X):
+        """
+         Forward propagation of the neural network.
+         @param X - ( n d ) array of data
+         @return ( n ) array of activation of neuron at each
+        """
+        Z1 = np.dot(self.__W1, X) + self.__b1
+        self.__A1 = 1 / (1 + np.exp(-Z1))
+        Z2 = np.dot(self.__W2, self.__A2) + self.__b2
+        self.__A2 = 1 / (1 + np.exp(-Z2))
+        return self.__A1, self.__A2
