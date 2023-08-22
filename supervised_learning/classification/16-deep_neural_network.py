78119b0a91f683dd154ce9840df193c1786cc676
Mathys <mathys.laurent3164@gmail.com>
2023-08-22 13:24:54 +0000

    len condition

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -12,7 +12,7 @@ class DeepNeuralNetwork:
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        if not layers:
+        if not layers or not isinstance(layers, list):
             raise TypeError("layers must be a list of positive integers")
 
         self.L = len(layers)
