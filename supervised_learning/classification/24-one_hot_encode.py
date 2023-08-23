38241fd1c2eb45a90a7b7f9b9a3698af89cf3f4f
Mathys <mathys.laurent3164@gmail.com>
2023-08-23 15:01:33 +0000

    add encode in hot one and decode func

---
+++ supervised_learning/classification/24-one_hot_encode.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+
+def one_hot_encode(Y, classes):
+    """
+     One hot encode the labels.
+     @param Y -  array of class labels
+     @param classes - number of classes in the training set
+     @return - array of one hot
+    """
+    try:
+        m = Y.shape[0]
+        encoded_labels = np.zeros((classes, m))
+
+        # Set the encoded labels for each class label.
+        for i in range(m):
+            class_label = Y[i]
+            encoded_labels[class_label, i] = 1
+
+        return encoded_labels
+    except:
+        return None
---
+++ supervised_learning/classification/25-one_hot_decode.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+
+def one_hot_decode(one_hot):
+    """
+     Decodes one hot encoding.
+     @param one_hot - A 2D NumPy array of shape
+     @return A 1D NumPy array of shape
+    """
+    try:
+        decoded_labels = np.argmax(one_hot, axis=0)
+        return decoded_labels
+    except:
+        return None
