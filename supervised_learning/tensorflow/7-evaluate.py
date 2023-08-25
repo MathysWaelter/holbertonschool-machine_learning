54614ba137158b66173daf8f14e641bcb27e7653
Mathys <mathys.laurent3164@gmail.com>
2023-08-25 18:45:42 +0200

    delete import

--- supervised_learning/tensorflow/7-evaluate.py
+++ supervised_learning/tensorflow/7-evaluate.py
@@ -1,32 +1,25 @@
 #!/usr/bin/env python3
-"""forward_prop"""
+"""evaluate"""
 
 
 import tensorflow.compat.v1 as tf
 
-calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
-calculate_loss = __import__('4-calculate_loss').calculate_loss
-create_placeholders = __import__('0-create_placeholders').create_placeholders
-forward_prop = __import__('2-forward_prop').forward_prop
-
-
 def evaluate(X, Y, save_path):
     """
-    Evaluates the output of a neural network.
+    Evaluate a saved model
     """
-    tf.reset_default_graph()
-    x, y = create_placeholders(X.shape[1], Y.shape[1])
-    y_pred = forward_prop(x, [], [])
-    loss = calculate_loss(y, y_pred)
-    accuracy = calculate_accuracy(y, y_pred)
+    with tf.Session() as session:
+        saver = tf.train.import_meta_graph(save_path + ".meta")
+        saver.restore(session, save_path)
 
-    saver = tf.train.Saver()
+        x = tf.get_collection('x')[0]
+        y = tf.get_collection('y')[0]
+        y_pred = tf.get_collection('y_pred')[0]
+        accuracy = tf.get_collection('accuracy')[0]
+        loss = tf.get_collection('loss')[0]
 
-    with tf.Session() as sess:
-        saver.restore(sess, save_path)
+        prediction = session.run(y_pred, feed_dict={x: X, y: Y})
+        accuracy = session.run(accuracy, feed_dict={x: X, y: Y})
+        loss = session.run(loss, feed_dict={x: X, y: Y})
 
-        prediction = sess.run(y_pred, feed_dict={x: X, y: Y})
-        acc = sess.run(accuracy, feed_dict={x: X, y: Y})
-        cost = sess.run(loss, feed_dict={x: X, y: Y})
-
-    return prediction, acc, cost
+    return prediction, accuracy, loss
