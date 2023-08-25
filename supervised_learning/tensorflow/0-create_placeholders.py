698fbfd07bfd33a00d098dd9864d48a0b564555c
Mathys <mathys.laurent3164@gmail.com>
2023-08-25 18:03:25 +0200

    pycodestyl

--- supervised_learning/tensorflow/0-create_placeholders.py
+++ supervised_learning/tensorflow/0-create_placeholders.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
+"""place     holder"""
 
 import tensorflow.compat.v1 as tf
+
+
 def create_placeholders(nx, classes):
+    """create a placeholder"""
 
     x = tf.placeholder(tf.float32, shape=[None, nx], name='x')
     y = tf.placeholder(tf.float32, shape=[None, classes], name='y')
