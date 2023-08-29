7627ce20cd8eaa3fe2702904d4c8efe3cb66ee17
Mathys <mathys.laurent3164@gmail.com>
2023-08-29 11:59:43 +0200

    shuffle

--- supervised_learning/optimization/2-shuffle_data.py
+++ supervised_learning/optimization/2-shuffle_data.py
@@ -6,6 +6,7 @@ import numpy as np
 
 def shuffle_data(X, Y):
     """permutation"""
-    X = np.random.permutation(X)
-    Y = np.random.permutation(Y)
-    return X, Y
+    permutation = np.random.permutation(X.shape[0])
+    X_shuffled = X[permutation]
+    Y_shuffled = Y[permutation]
+    return X_shuffled, Y_shuffled
